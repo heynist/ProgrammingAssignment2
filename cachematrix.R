1c3b0cae80cd5da7390598be1cb33ba6b695007a
Steven Heyninck <steven.heyninck@cronos.be>
2014-08-23 13:41:21 +0200

    added extra comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix : takes a matrix and wraps it in a list of 4 functions
+## cacheSolve : takes the output list of the makeCacheMatrix function
+## and returns the inverse of the original matrix (using a cached value of it when possible)
 
 ## Write a short comment describing this function
 ## This function takes a matrix
@@ -7,21 +10,27 @@
 ## to get and set the matrix,
 ## and to get and set its inverse (which is cached locally)
 makeCacheMatrix <- function(x = matrix()) {
+  ## initialize the cache for the inverse of the matrix
   i <- NULL
   
+  ## explicit call to set the matrix (consequently we need to reset the cached value for the inverse of it)
   setMatrix <- function(matrix) {
     x <<- matrix
     i <<- NULL
   }
   
+  ## return the original matrix
   getMatrix <- function() x
   
+  ## cache the inverse of the matrix
   setInverse <- function(inverse) {
     i <<- inverse
   }
   
+  ## return the inverted matrix
   getInverse <- function() i
   
+  ## list the 4 functions
   list(setMatrix = setMatrix, 
        getMatrix = getMatrix, 
        setInverse = setInverse, 
